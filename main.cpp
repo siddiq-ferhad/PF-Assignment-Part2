be73860311a01306cb4d95a556c5a9969b031b4b
LINGCHEEXIANG <123733132+LINGCHEEXIANG@users.noreply.github.com>
2023-02-05 22:25:59 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -187,7 +187,7 @@ int main()
     char finaldeci = tolower(deci);
     // define array of characters
     char characters[11] = {' ', ' ', '^', 'v', '>', '<', 'h', 'p', 'r', ' '};
-    vector<int>numofzombie;
+    
     if (finaldeci == 'n')
     {
         rows = 7;
